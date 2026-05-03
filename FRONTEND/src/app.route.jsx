0d50cec35ff3862451ef6a9f7d7489ae8e7374ad
Soumya Nandi <soumyanandi2003@gmail.com>
2026-05-03 21:36:19 +0530

    Creating the route for interview page in frontend

--- FRONTEND/src/app.route.jsx
+++ FRONTEND/src/app.route.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
+import Interview from "./features/interview/pages/Interview";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home";
 
@@ -17,4 +18,8 @@ export const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
   },
+  {
+    path: "/interview",
+    element: <Protected> <Interview/>    </Protected>,
+  }
 ]);
