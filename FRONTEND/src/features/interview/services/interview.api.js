699c0f8010aafe8a63f8476751255f03d1965d9a
Soumya Nandi <soumyanandi2003@gmail.com>
2026-05-04 13:31:44 +0530

    Creating the api layer in frontend to communicate with backend, using three function generateInterviewReport getInterviewReportById  getAllReportsForUser

---
+++ FRONTEND/src/features/interview/services/interview.api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+});
+
+//function to call the handleGenerateInterviewReport controller for generating the report using the AI
+export const generateInterviewReport = async ({jobDescription , selfDescription, resumeFile}) => {
+  const formData = new FormData();
+  formData.append("jobDescription", jobDescription);
+  formData.append("selfDescription", selfDescription);
+  formData.append("resume", resumeFile);
+
+  const response = await api.post("/api/interview", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  return response.data;
+}
+
+//function to call the handleGetInterviewReportById controller for fetching any specific report based on their interviewId
+export const getInterviewReportById = async (req,res)=>{
+  const response = await api.get(`/api/interview/report/${req.interviewId}`);
+
+
+  return response.data
+}
+
+
+//function to call the handleGetAllInterviewReportsForUser controller for fetching all reports for a specific user
+export const getAllReportsForUser = async (req,res)=>{
+  const response = await api.get("/api/interview/all-reports");
+
+  return response.data
+}
+
