b9baf24fb7e8735c28c2b1147b5a7f00f0aba39c
Soumya Nandi <soumyanandi2003@gmail.com>
2026-05-15 23:57:46 +0530

    Working on the navbar

--- FRONTEND/src/features/auth/components/Navbar.jsx
+++ FRONTEND/src/features/auth/components/Navbar.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 import logo from "../../../asset/logo.png"
 const Navbar = () => {
   return (
-<nav className="backdrop-blur-lg  bg-[#0d1117] border-b border-white/10 flex items-center py-4 px-10 w-full z-50">
-        <h1 className="text-md font-bold ">HIREMIND-<span className='text-pink-500'>AI</span> </h1>
+<nav className="backdrop-blur-lg  bg-[#0d1117] border-b border-white/10 flex justify-between items-center py-4 px-10 w-full z-50">
+        <h1 className="text-md font-bold ">HireMind-<span className='text-pink-500'>AI</span> </h1>
 
         <div className="hidden md:flex gap-8 text-gray-300">
+          <Link to={"/"} >Home</Link>
           <Link to="/login">Login</Link>
-          <Link to="/home" >Home</Link>
+          <Link to="/home" >Generate Report</Link>
           
         </div>
 
