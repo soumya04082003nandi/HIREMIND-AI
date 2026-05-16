b3f475b2320b78294a18578608acf87c16063fc6
Soumya Nandi <soumyanandi2003@gmail.com>
2026-05-16 15:50:33 +0530

    compeleting the structure of navbar, adding the smmoth scroll in my-reports section

--- FRONTEND/src/features/auth/components/Navbar.jsx
+++ FRONTEND/src/features/auth/components/Navbar.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../../../asset/logo.png"
+import { useAuth } from '../hooks/useAuth'
 const Navbar = () => {
+
+  const { user } = useAuth()
+
+
   return (
-<nav className="backdrop-blur-lg  bg-[#0d1117] border-b border-white/10 flex justify-between items-center py-4 px-10 w-full z-50">
-        <h1 className="text-md font-bold ">HireMind-<span className='text-pink-500'>AI</span> </h1>
+    <nav className="backdrop-blur-lg  bg-[#0d1117] border-b border-white/10 flex justify-between items-center py-4 px-10 w-full z-50">
+      <h1 className="text-md font-bold ">HireMind-<span className='text-pink-500'>AI</span> </h1>
 
-        <div className="hidden md:flex gap-8 text-gray-300">
-          <Link to={"/"} >Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/home" >Generate Report</Link>
-          
-        </div>
+      <div className="hidden md:flex gap-8 text-gray-300">
+        <Link to={"/"} className='cursor-pointer hover:text-pink-500 transition duration-300' >Home</Link>
+        <Link to="/home" className='cursor-pointer hover:text-pink-500 transition duration-300' >Generate Report</Link>
+        {!user && <Link to="/login" className='cursor-pointer hover:text-pink-500 transition duration-300'>Login</Link>}
+        {user && <Link to={"/home"} className='cursor-pointer hover:text-pink-500 transition duration-300'>test</Link>}
 
-       
-      </nav>
+      </div>
+
+
+    </nav>
 
   )
 }
