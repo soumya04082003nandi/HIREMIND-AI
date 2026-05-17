296038614f2c357d6d8d1857b505565cd783d8ae
Soumya Nandi <soumyanandi2003@gmail.com>
2026-05-17 23:51:09 +0530

    updating the base url

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParesr())
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://hiremind-ai-f33f.onrender.com",
     credentials:true
 }))
 
