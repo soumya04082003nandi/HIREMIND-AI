84297db9a66dc584ec304aed7cb4e704901f38ee
Soumya Nandi <soumyanandi2003@gmail.com>
2026-06-17 14:53:26 +0530

    Navigation logic in Errorpopup component

--- FRONTEND/src/features/auth/components/Errorpopup.jsx
+++ FRONTEND/src/features/auth/components/Errorpopup.jsx
@@ -33,6 +33,7 @@ const Errorpopup = ({ onClose }) => {
   return (
     <div onClick={(e) => {
       setPopOpen(false)
+      navigate(`/interview/report/${report._id}`)
 
     }}
       className={`fixed inset-0 z-50  items-center ${!popOpen ? 'hidden' : 'flex'} justify-center bg-black/70 backdrop-blur-sm p-4`}>
@@ -49,7 +50,7 @@ const Errorpopup = ({ onClose }) => {
           <X size={20}
             onClick={(e) => {
               setPopOpen(false)
-            
+              navigate(`/interview/report/${report._id}`)
             }}
           />
         </button>
@@ -101,6 +102,7 @@ const Errorpopup = ({ onClose }) => {
         <button
           onClick={(e) => {
             setPopOpen(false)
+            navigate(`/interview/report/${report._id}`)
 
           }}
           className="mt-8 w-full rounded-xl bg-pink-700 py-3 text-lg font-semibold text-white transition hover:bg-[#ff1d45] active:scale-95 shadow-[0_0_20px_rgba(255,45,85,0.35)]"
