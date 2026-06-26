5a204a1aaf3eb94b709b1a390f1facc3cfab371c
Soumya Nandi <soumyanandi2003@gmail.com>
2026-06-26 23:40:28 +0530

    Removing the unecessary strong password checking logic from login page

--- FRONTEND/src/features/auth/pages/Login.jsx
+++ FRONTEND/src/features/auth/pages/Login.jsx
@@ -11,12 +11,10 @@ const Login = () => {
   const [password, setPassword] = useState("")
   const [showPass, setShowPass]=useState(false)
 
-  const passwordRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])[A-Za-z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{8,}$/;
+  
 
   const handleSubmit = async(e)=>{
-    e.preventDefault(); 
-    // console.log(passwordRegex.test(password));
-    
+    e.preventDefault();     
       const user =await handleLogin({email,password})
     if(user)navigate("/home")      
         
